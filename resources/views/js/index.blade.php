52bb08e3a64ea657bd94af9d3a592e17dbf49b1e
CJMario89 <a7654321000@gmail.com>
2022-02-06 14:25:57 +0800

    fix-1

---
+++ resources/views/js/index.blade.php
@@ -0,0 +1,22 @@
+<script>
+    var cubicSvg = document.querySelector(".cubicSvg");
+    if(window.innerWidth < 768){
+        cubicSvg.addEventListener("touchmove", function(){
+            cubicSvg.style.animationPlayState = "paused";
+        });
+        cubicSvg.addEventListener("touchstart", function(){
+            cubicSvg.style.animationPlayState = "running";
+        });
+        cubicSvg.addEventListener("touchend", function(){
+            cubicSvg.style.animationPlayState = 'running';
+        });
+    }else{
+        cubicSvg.addEventListener("mouseenter", function(){
+            cubicSvg.style.animationPlayState = 'paused';
+        });
+        cubicSvg.addEventListener("mouseleave", function(){
+            cubicSvg.style.animationPlayState = 'running';
+        });
+    }
+    
+</script> 
