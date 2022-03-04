816cbd55d460fc96a6f658e421ff0c9d118c7ccd
CJMario89 <a7654321000@gmail.com>
2022-03-04 21:59:12 +0800

    Update footer.blade.php

--- resources/views/js/components/footer.blade.php
+++ resources/views/js/components/footer.blade.php
@@ -1,10 +1,10 @@
 <script type='text/javascript' defer='true'>
 	var fb = document.querySelector("#fb");
     fb.addEventListener("click", function(){
-        window.location = 'https://www.facebook.com/mariochenx/';
+        window.location = 'https://www.facebook.com/Toolsbox-107904881842226/';
     });
     var ig = document.querySelector("#ig");
     ig.addEventListener("click", function(){
-        window.location = 'https://www.instagram.com/c.j.m.a.r.i.o/';
+        window.location = 'https://www.instagram.com/toolsbox_web/';
     });
 </script>
