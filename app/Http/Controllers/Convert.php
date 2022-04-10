0a3a408a44324946cd4a74b6753f84fb320f835a
cjmario89 <a7654321000@gmail.com>
2022-04-10 23:30:13 +0800

    Update Convert.php

--- app/Http/Controllers/Convert.php
+++ app/Http/Controllers/Convert.php
@@ -20,7 +20,7 @@ class Convert extends Controller
                 foreach($files as $file){
                     $fileName = $file->getSize();//using size as temporary filename //avoid race condition
                     $path = $file->storeAs('/public', $fileName, 'local');
-                    $convert = "(cd ../storage/app/public/tmp && libreoffice --infilter=='writer_pdf_import' --headless --convert-to ".$type.":'writer_pdf_Export' ".$fileName.")";
+                    $convert = "(cd ../storage/app/public && export HOME=/var/www/toolsbox/storage/app/public && libreoffice --infilter=='writer_pdf_import' --headless --convert-to ".$type.":'writer_pdf_Export' ".$fileName.")";
                     shell_exec($convert);
                     return response()->json(['message','file not valid'], 200);
 
