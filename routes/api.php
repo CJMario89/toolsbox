fc20c8a5848ec2e8a196beaf17b096d9c2d68955
cjmario89 <a7654321000@gmail.com>
2022-04-10 22:01:49 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/fileConvert/convert', [Convert::class, "convert"]);
+Route::any('/fileConvert/convert', [Convert::class, "convert"]);
