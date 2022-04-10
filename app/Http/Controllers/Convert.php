935fcbb57fe388f70ec84a48dd4f26c55dbb05f7
cjmario89 <a7654321000@gmail.com>
2022-04-10 22:36:17 +0800

    Update Convert.php

--- app/Http/Controllers/Convert.php
+++ app/Http/Controllers/Convert.php
@@ -20,7 +20,7 @@ class Convert extends Controller
                 foreach($files as $file){
                     $fileName = $file->getSize();//using size as temporary filename //avoid race condition
                     $path = $file->storeAs('/public', $fileName, 'local');
-                    $convert = "(cd ../storage/app/public && libreoffice --infilter=='writer_pdf_import' --headless --convert-to ".$type.":'writer_pdf_Export' ".$fileName.")";
+                    $convert = "(cd ../storage/app/public && export HOME=/tmp && libreoffice --infilter=='writer_pdf_import' --headless --convert-to ".$type.":'writer_pdf_Export' --outdir ./tmp ".$fileName.")";
                     shell_exec($convert);
                     //shell_exec("(cd ../storage/app/public && rm ".$fileName.")");
                     $convertedfileName = explode('.', $file->getClientOriginalName())[0].".".$type;
