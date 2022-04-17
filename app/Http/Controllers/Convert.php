81974ea072d8932e88e43c777a6feffeb2d7f371
cjmario89 <a7654321000@gmail.com>
2022-04-17 22:29:23 +0800

    update

--- app/Http/Controllers/Convert.php
+++ app/Http/Controllers/Convert.php
@@ -51,16 +51,17 @@ class Convert extends Controller
             $i = 0;
             foreach($files as $file){
                 $dir = $this->getRandomString();
-                $fileName = explode('.', $file->getClientOriginalName())[0];
+                $fileName = $file->getClientOriginalName();
+                $fileName = $this->fileNamePreProcess($fileName);
                 $path = $file->storeAs('/public'."/".$dir, $fileName, 'local');
                 $from = $this->getFromFileFormate(explode('.', $file->getClientOriginalName())[1]);
                 $to = $this->getToFileFormate($type);
-                $convert = "(cd ../storage/app/public/".$dir." && export HOME=/var/www/toolsbox/storage/app/public/".$dir." && libreoffice --infilter='".$from."' --headless --convert-to ".$type.":'".$to."' ".$fileName.")";
+                $convert = "(cd ../storage/app/public/".$dir." && export HOME=/var/www/toolsbox/storage/app/public/".$dir." && libreoffice --infilter='".$from."' --headless --convert-to ".$type.":'".$to."' '".$fileName."')";
                 shell_exec($convert);
-                shell_exec("(cd ../storage/app/public/".$dir." && rm ".$fileName.")");
-                $convertedfileName = $fileName.".".$type;
+                //shell_exec("(cd ../storage/app/public/".$dir." && rm '".$fileName."')");
+                $convertedfileName = explode('.', $file->getClientOriginalName())[0].".".$type;
                 //$convertedFile = file_get_contents(__DIR__."/../../../storage/app/public/".$fileName.".".$type);
-                $convertedFile = url('/storage')."/".$dir."/".$fileName.".".$type;
+                $convertedFile = url('/storage')."/".$dir."/".explode('.', $file->getClientOriginalName())[0].".".$type;
 
                 $converted[$i]["fileName"] = $convertedfileName;
                 $converted[$i]["file"] = $convertedFile;
@@ -83,11 +84,17 @@ class Convert extends Controller
         return $rand;
     }
 
+    private function fileNamePreProcess($fileName){
+        //for shell exec
+        $fileName = str_replace("'", "\"", $fileName);
+        return $fileName;
+    }
+
     private function getToFileFormate($type){
         if($type == "pdf"){
             return "writer_pdf_Export";
         }else if($type == "docx"){
-            return "Office Open XML Text";
+            return "MS Word 97";
         }else if($type == "odf"){
             return "math8";
         }
@@ -97,7 +104,7 @@ class Convert extends Controller
         if($type == "pdf"){
             return "writer_pdf_import";
         }else if($type == "docx"){
-            return "Office Open XML Text";
+            return "MS Word 97";
         }else if($type == "odf"){
             return "math8";
         }
