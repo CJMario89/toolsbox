60166b1ff0c7260ae6bd162f4e6678b77501eb7f
cjmario89 <a7654321000@gmail.com>
2022-04-24 11:53:09 +0800

    Update PDFToWord.blade.php

--- resources/views/js/PDFToWord.blade.php
+++ resources/views/js/PDFToWord.blade.php
@@ -86,7 +86,7 @@
     //response pdf, odf, doc(x) //converting
     //display //done
     async function post_file(){
-        type = "pdf";
+        type = "doc";
         Type = fileTypes[0];
         const url = "{{url('/api/PDFToWord/convert')}}" + "?type=" + type;   
         progress.innerHTML = "{{__('PDFToWord.converting')}}";
